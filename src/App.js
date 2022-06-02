2a7181c3138b51d820fa4968663c9fd8883affff
BlaineR41 <blaineruttan@gmail.com>
2022-06-02 19:33:07 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
+import SearchForm from './components/SearchForm'
+import Nav from './components/Nav'
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import apiKey from './config'
+
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        results: [],
+        arizona: [],
+        utah: [],
+        pnw: []
+      };
+    }
+    
+  componentDidMount() {
+    fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&sort=relevance&format=json&nojsoncallback=1')
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({results: responseData.data });
+    })
+    .catch(error =>{
+      console.log('error fetching and parsing data', error)
+    });
+  }
+
   render() {
+    console.log(this.state.results);
     return (
-      <div>App</div>
+      <BrowserRouter>
+        <SearchForm />
+        <Nav />
+      </BrowserRouter>
     )
   }
 }
